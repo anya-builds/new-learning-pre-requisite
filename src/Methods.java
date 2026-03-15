da130722c037e573518d07e46290f87a75182362
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-15 19:36:51 +0530

    methods

--- src/Methods.java
+++ src/Methods.java
@@ -2,12 +2,18 @@ public class Methods {
     public static void main(String[] args) {
 
 
-        sayHello();
-        sayHello();
-        sayHello();
+        sayHello("Ankita");
+        sayHello("Manasvi");
+        sayHello("Parul");
+
+        System.out.println("Addition: " + add(29,80));
     }
-    private static void sayHello(){
-        System.out.println("Hello");
+    private static void sayHello(String name){
+        System.out.println("Hello, " + name);
         System.out.println("Welcome to Skywards");
+    }
+
+    private static int add(int a,int b){
+        return a+b;
     }
 }

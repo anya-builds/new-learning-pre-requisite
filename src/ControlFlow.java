c1891cc803dff774df4db65615c91d186642533d
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-15 19:26:19 +0530

    java

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -72,5 +72,14 @@ public class ControlFlow {
         for (int i=0;i< fruits.length;i++){
             System.out.println(fruits[i]);
         }
+
+        //jump statement
+        //break
+
+        for(int i=0;i<10;i++){
+            if(i==3) continue;
+            if(i==7) break;
+            System.out.println("Count: "+i);
+        }
     }
 }

79bfd003ff0a7e7cd2327a634c378114d1ec348a
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-15 19:33:34 +0530

    methods

---
+++ src/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+    public static void main(String[] args) {
+
+
+        sayHello();
+        sayHello();
+        sayHello();
+    }
+    private static void sayHello(){
+        System.out.println("Hello");
+        System.out.println("Welcome to Skywards");
+    }
+}

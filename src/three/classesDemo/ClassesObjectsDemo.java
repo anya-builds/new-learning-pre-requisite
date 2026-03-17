a7f3891aef47f61d067e31807da1bdb5c48433fe
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-17 22:42:58 +0530

    methods

--- src/three/classesDemo/ClassesObjectsDemo.java
+++ src/three/classesDemo/ClassesObjectsDemo.java
@@ -5,6 +5,14 @@ public class ClassesObjectsDemo {
         Car car1= new Car();
         car1.speed = 100;
         car1.brand = "Tesla";
-        
+        car1.color = "Red";
+        car1.drive();
+
+        Car car2 = new Car();
+        car2.brand= "Toyota";
+        car2.color = "Black";
+        car2.speed = 679;
+        car2.drive();
+
     }
 }

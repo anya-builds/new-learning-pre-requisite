693b0be7a602b16b15580245a929ea72f534336e
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-18 06:09:18 +0530

    methods

--- src/constructors/Car.java
+++ src/constructors/Car.java
@@ -6,8 +6,11 @@ public class Car {
     String color;
     int speed;
 
-    public Car(){
+    public Car(String brand,String color,int speed){
         System.out.println("Constructor Called.");
+        this.brand = brand;
+        this.color = color;
+        this.speed = speed;
     }
 
     public void drive(){
--- src/constructors/ConstructorDemo.java
+++ src/constructors/ConstructorDemo.java
@@ -2,13 +2,13 @@ package constructors;
 
 public class ConstructorDemo {
    public static void main(String[] args) {
-        Car car1= new Car();
+        Car car1= new Car("Toyota","yellow",673);
 //        car1.speed = 100;
 //        car1.brand = "Tesla";
 //        car1.color = "Red";
         car1.drive();
 
-        Car car2 = new Car();
+        Car car2 = new Car("Tesla","red",89);
 //        car2.brand= "Toyota";
 //        car2.color = "Black";
 //        car2.speed = 679;

68b58283677aeb8efad0151b5d27ecf46d0cb7f9
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-18 06:22:11 +0530

    methods

--- src/constructors/Car.java
+++ src/constructors/Car.java
@@ -6,14 +6,27 @@ public class Car {
     String color;
     int speed;
 
-    public Car(String brand,String color,int speed){
-        System.out.println("Constructor Called.");
+    public Car(String brand, int speed, String color) {
         this.brand = brand;
-        this.color = color;
         this.speed = speed;
+        this.color = color;
     }
+
+    //    public Car(String brand,String color,int speed){
+//        System.out.println("Constructor Called.");
+//        this.brand = brand;
+//        this.color = color;
+//        this.speed = speed;
+//    }
 
     public void drive(){
         System.out.println(brand + " is driving at " + speed);
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed){
+        this.speed=  speed;
     }
 }

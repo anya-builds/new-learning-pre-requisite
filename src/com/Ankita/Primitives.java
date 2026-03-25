6281312f2756d2303bd30ea807effe261cc1bae3
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-26 04:39:15 +0530

    basic

---
+++ src/com/Ankita/Primitives.java
@@ -0,0 +1,19 @@
+package com.Ankita;
+
+import java.util.Scanner;
+
+public class Primitives {
+    public static void main(String[] args) {
+//        int rollno = 64;
+//        String name = "Ankita";
+//        char letter = 't';
+//        float marks = 89.73f;
+//        double largeDecimalNumber = 763298;
+//        long largeInteger = 973269328780352703L;
+//        boolean check = true;
+        Scanner sc = new Scanner(System.in);
+        int rl = sc.nextInt();
+        System.out.println("Your roll no is "+ rl);
+
+    }
+}

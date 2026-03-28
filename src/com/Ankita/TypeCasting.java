23863526bf9326d98fefd9d95385726f61b6119f
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-28 10:54:53 +0530

    type casting

--- src/com/Ankita/TypeCasting.java
+++ src/com/Ankita/TypeCasting.java
@@ -6,8 +6,26 @@ import java.util.Scanner;
 public class TypeCasting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float num = sc.nextFloat();
-        System.out.println(num);
+//        float num = sc.nextFloat();
+//        System.out.println(num);
+
+        int num =(int) 67.56f;
+//        System.out.println(num);
+
+        //automatic type promotion in expression
+//        int a = 257;
+//
+//        byte b=(byte) a;
+//        byte a=40;
+//        byte b=50;
+//        byte c=100;
+//        int d=(a*b)/c;
+//        System.out.println(d);
+
+//        byte b=50;
+//        b=b*2;
+        int number = 'A';
+        System.out.println("你好");
     }
 
 }

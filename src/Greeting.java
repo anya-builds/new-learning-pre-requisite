17b62eafb583066beebd965fb94fc16aff0055a5
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-29 12:36:29 +0530

    swapping  number

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
-        greeting();
+//        greeting();
+//        String personalized = myGreet("Ankita Arya");
+//        System.out.println(personalized);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name=sc.next();
+        String personalized = myGreet(name);
+        System.out.println(personalized);
     }
-    static void greeting(){
+
+    static String myGreet(String name) {
+        String message = "Hello " + name;
+        return message;
+
+    }
+
+    static void greet(){
         System.out.println("Hello world!");
     }
 }
--- src/Sum.java
+++ src/Sum.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        int ans=sum2();
+//        int ans=sum2();
+        int ans=sum3(12,34);
         System.out.println(ans);
+    }
+    static int sum3(int a,int b){
+        int sum=a+b;
+        return sum;
     }
     static int sum2(){
         Scanner sc=new Scanner(System.in);

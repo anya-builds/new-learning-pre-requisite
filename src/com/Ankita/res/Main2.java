9e9870020ea33c925231e049cbff8add80eaeaba
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-29 11:23:49 +0530

    Switch Statement

--- src/com/Ankita/res/Main2.java
+++ src/com/Ankita/res/Main2.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String fruit=sc.next();
+//        String fruit=sc.next();
 
-        switch (fruit) {
-            case "Mango" -> System.out.println("King of fruits");
-            case "Apple" -> System.out.println("A sweet red fruit");
-            case "Orange" -> System.out.println("Round fruit");
-            case "Grapes" -> System.out.println("small fruit");
-            default -> System.out.println("please enter  valid fruit");
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of fruits");
+//            case "Apple" -> System.out.println("A sweet red fruit");
+//            case "Orange" -> System.out.println("Round fruit");
+//            case "Grapes" -> System.out.println("small fruit");
+//            default -> System.out.println("please enter  valid fruit");
+
+        int day=sc.nextInt();
+        switch (day){
+            case 1-> System.out.println("Monday");
+            case 2-> System.out.println("Tuesday");
+            case 3-> System.out.println("Wednesday");
+            case 4-> System.out.println("Thursday");
+            case 5-> System.out.println("Friday");
+            case 6-> System.out.println("Saturday");
+            case 7-> System.out.println("Sunday");
+//            default -> System.out.println("Invalid day");
         }
     }
 }

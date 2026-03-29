3082de75bdc017f4c63aa6b4417af068ac873802
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-29 23:42:24 +0530

    function overloading

---
+++ src/OverLoading.java
@@ -0,0 +1,13 @@
+public class OverLoading {
+    public static void main(String[] args) {
+        fun(34);
+        fun("Ankita");
+    }
+    static void fun(int a){
+        System.out.println(a);
+    }
+    static void fun(String name){
+        System.out.println(name);
+
+    }
+}
--- src/VarArgs.java
+++ src/VarArgs.java
@@ -2,8 +2,12 @@ import java.util.Arrays;
 
 public class VarArgs {
     public static void main(String[] args) {
-        fun(12,7,7,3,37);
+//        fun(12,7,7,3,37);
 
+        multiple(12,34,"Ankita","Gagan");
+    }
+    static void multiple(int a,int b,String ...v){
+//        System.out.println(Arrays.toString(a,b,v));
     }
     static void fun(int ...v){
         System.out.println(Arrays.toString(v));

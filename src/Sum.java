91e366d5d6d8805acfae7e50ed001a20c11569ae
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-03-29 12:03:30 +0530

    sum of number

--- src/Sum.java
+++ src/Sum.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+        int ans=sum2();
+        System.out.println(ans);
+    }
+    static int sum2(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1=sc.nextInt();
+        System.out.println("Enter number2: ");
+        int num2=sc.nextInt();
+        int sum2=num2+num1;
+//        System.out.println("The sum= "+ sum);
+        return sum2;
     }
     static void sum(){
         Scanner sc=new Scanner(System.in);

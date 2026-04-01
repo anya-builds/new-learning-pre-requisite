ab54637e8a76a6c991d59b5d4da77780ac053274
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-01 22:09:28 +0530

    Array in java

--- src/com/Ankita/Arrays/Main.java
+++ src/com/Ankita/Arrays/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int[] ros; //declaration of array. ros is getting defined in the stack
         ros= new int[5]; // initial actually here object is being created in the memory
-        System.out.println(ros[1]);
+//        System.out.println(ros[1]);
 
         String[] arr=new String[4];
         System.out.println(arr[2]);
+
 
     }
 }
---
+++ src/com/Ankita/res/Input.java
@@ -0,0 +1,15 @@
+package com.Ankita.res;
+
+public class Input {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=23;
+        arr[1]=45;
+        arr[2]=25;
+        arr[3]=34;
+        arr[4]=78;
+        for (int i=0;i<5;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}

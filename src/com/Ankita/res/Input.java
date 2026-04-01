d996f36b7034293181a5bdd26cc98c14a1f1b5a0
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-01 22:17:39 +0530

    Array in java

--- src/com/Ankita/res/Input.java
+++ src/com/Ankita/res/Input.java
@@ -1,15 +1,23 @@
 package com.Ankita.res;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Input {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         int[] arr=new int[5];
-        arr[0]=23;
-        arr[1]=45;
-        arr[2]=25;
-        arr[3]=34;
-        arr[4]=78;
-        for (int i=0;i<5;i++) {
-            System.out.print(arr[i]+" ");
+//        arr[0]=23;
+//        arr[1]=45;
+//        arr[2]=25;
+//        arr[3]=34;
+//        arr[4]=78;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+//            System.out.println(arr[i]);
+
         }
+        System.out.println(Arrays.toString(arr));
     }
 }

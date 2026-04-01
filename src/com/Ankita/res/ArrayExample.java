3362a86f43a7fff124da156cd258cf42cb2825bb
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-02 02:54:34 +0530

    ArrayList

---
+++ src/com/Ankita/res/ArrayExample.java
@@ -0,0 +1,18 @@
+package com.Ankita.res;
+
+import java.util.ArrayList;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> lis=new ArrayList<>(10);
+        lis.add(67);
+        lis.add(63);
+        lis.add(23);
+        lis.add(32);
+        lis.add(2983);
+//        System.out.println(lis);
+//        for (int i=0;i<5;i++){
+//            System.out.println(lis.get(i));
+//        }
+    }
+}

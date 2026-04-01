b2c26ad0efa640ec586bc0678a7cada4ba856f6e
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-02 03:21:18 +0530

    reverse

--- src/com/Ankita/res/Swap.java
+++ src/com/Ankita/res/Swap.java
@@ -6,7 +6,19 @@ public class Swap {
     public static void main(String[] args) {
         int[] arr={1,3,123,9,18};
         swap(arr,1,3);
+//        System.out.println(Arrays.toString(arr));
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while (start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
     }
     static void swap(int[] arr,int index1,int index2){
         int temp=arr[index1];

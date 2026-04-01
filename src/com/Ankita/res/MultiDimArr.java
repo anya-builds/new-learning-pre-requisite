3427d250537e130e7ec184fa4e45d7ceb0ebc2cb
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-02 02:26:08 +0530

    Multi Dimensional Array

---
+++ src/com/Ankita/res/MultiDimArr.java
@@ -0,0 +1,39 @@
+package com.Ankita.res;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimArr {
+    public static void main() {
+        Scanner sc=new Scanner(System.in);
+//        int[][] arr=new int[3][];
+//        int[][] arr={
+//                {1,2,3},
+//                {4,5},
+//                {6,7,8,9}
+//        };
+        int[][] arr=new int[3][3];
+        System.out.println(arr.length);
+        //input
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col]=sc.nextInt();
+
+            }
+
+        }
+//        for (int row = 0; row < arr.length; row++) {
+//            for (int col = 0; col < arr[row].length; col++) {
+//                System.out.print(arr[row][col]+" ");
+//
+//            }
+//            System.out.println();
+//
+//        }
+        //output
+        for (int row = 0; row < arr.length; row++) {
+            System.out.println(Arrays.toString(arr[row]));
+        }
+
+    }
+}

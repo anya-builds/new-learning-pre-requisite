1af4a8658522cdf743f5346e22822fdee5e82dab
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-05 12:21:18 +0530

    Bubble Sort

---
+++ src/Binary/BubbleSort.java
@@ -0,0 +1,26 @@
+package Binary;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+      int[] arr={5,4,3,2,1};
+      bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            // for each step, max item will come at the last respective index
+            for (int j = 1; j < arr.length-i; j++) {
+                //swap if the item is smaller than previous item
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+
+            }
+            
+        }
+    }
+}
--- src/Binary/SearchInSorted.java
+++ src/Binary/SearchInSorted.java
@@ -1,61 +1,61 @@
-package Binary;
-
-public class SearchInSorted {
-    public static void main(String[] args) {
-            int[][] matrix={
-
-            };
-    }
-    static int[] binarySearch(int[][] matrix,int row,int cStart,int cEnd,int target){
-        while (cStart<=cEnd){
-         int mid=cStart+(cEnd-cStart)/1;
-         if(matrix[row][mid]==target){
-             return  new int[]{row,mid};
-         }
-         if(matrix[row][mid]<target){
-                cStart=mid+1;
-         }else{
-             cStart=mid-1;
-         }
-        }
-        return new int[]{-1,-1};
-    }
-    static int[] search(int[][] matrix,int target){
-        int rows=matrix.length;
-        int cols=matrix[0].length;
-        if(rows==1){
-            return binarySearch(matrix,0,0,cols-1,target);
-
-        }
-        int rStart=0;
-        int rEnd=rows-1;
-        int cMid=cols/2;
-        //run the loop till tw rows are remaining
-        while (rStart<(rEnd-1)){
-            int mid=rStart+(rEnd-rStart)/2;
-            if(matrix[mid][cMid]==target){
-                return new int[]{mid,cMid};
-            }
-            if(matrix[mid][cMid]<target){
-                rStart=mid;
-            }else{
-                rEnd=mid;
-            }
-        }
-        //now we have two rows
-        //check whether is in target is i the col of 2 rows
-        if(matrix[rStart][cMid]==target){
-            return new int[]{rStart,cMid};
-        }
-        if(matrix[rStart+1][cMid]==target){
-            return new int[]{rStart+1,cMid};
-        }
-
-        //search in first half
-        //search in second half
-        //search in third half
-        //search in fourth half
-
-
-    }
-}
+//package Binary;
+//
+//public class SearchInSorted {
+//    public static void main(String[] args) {
+//            int[][] matrix={
+//
+//            };
+//    }
+//    static int[] binarySearch(int[][] matrix,int row,int cStart,int cEnd,int target){
+//        while (cStart<=cEnd){
+//         int mid=cStart+(cEnd-cStart)/1;
+//         if(matrix[row][mid]==target){
+//             return  new int[]{row,mid};
+//         }
+//         if(matrix[row][mid]<target){
+//                cStart=mid+1;
+//         }else{
+//             cStart=mid-1;
+//         }
+//        }
+//        return new int[]{-1,-1};
+//    }
+//    static int[] search(int[][] matrix,int target){
+//        int rows=matrix.length;
+//        int cols=matrix[0].length;
+//        if(rows==1){
+//            return binarySearch(matrix,0,0,cols-1,target);
+//
+//        }
+//        int rStart=0;
+//        int rEnd=rows-1;
+//        int cMid=cols/2;
+//        //run the loop till tw rows are remaining
+//        while (rStart<(rEnd-1)){
+//            int mid=rStart+(rEnd-rStart)/2;
+//            if(matrix[mid][cMid]==target){
+//                return new int[]{mid,cMid};
+//            }
+//            if(matrix[mid][cMid]<target){
+//                rStart=mid;
+//            }else{
+//                rEnd=mid;
+//            }
+//        }
+//        //now we have two rows
+//        //check whether is in target is i the col of 2 rows
+//        if(matrix[rStart][cMid]==target){
+//            return new int[]{rStart,cMid};
+//        }
+//        if(matrix[rStart+1][cMid]==target){
+//            return new int[]{rStart+1,cMid};
+//        }
+//
+//        //search in first half
+//        //search in second half
+//        //search in third half
+//        //search in fourth half
+//
+//
+//    }
+//}

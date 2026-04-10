0142b69b06a3388ace235a684b8c8979a7b22d7e
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-10 20:53:25 +0530

    String

---
+++ src/stringDS/Comparison.java
@@ -0,0 +1,11 @@
+package stringDS;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a="Kunal";
+        String b="Kunal";
+
+        // ===
+        System.out.println(a==b);
+    }
+}
---
+++ src/stringDS/Main.java
@@ -0,0 +1,14 @@
+package stringDS;
+
+public class Main {
+    public static void main(String[] args) {
+        String name="Ankita Arya";
+        System.out.println(name);
+
+        String a="kunal";
+        System.out.println(a);
+        a="Arya";
+        System.out.println(a);
+    }
+
+}

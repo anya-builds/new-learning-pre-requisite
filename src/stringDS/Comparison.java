2519ff0b00a2f8ffc1d9bdd748d10f00cea0de52
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-10 21:17:45 +0530

    String

--- src/stringDS/Comparison.java
+++ src/stringDS/Comparison.java
@@ -11,6 +11,7 @@ public class Comparison {
         String name2= new String("Kunal");
 //        System.out.println(name1==name2);
         System.out.println(name1.equals(name2));
+        System.out.println(name1.charAt(0));
     }
 
 }
---
+++ src/stringDS/Output.java
@@ -0,0 +1,11 @@
+package stringDS;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+        System.out.println("Ankita");
+        System.out.println(new int[]{1,2,4,5,3});
+        String name=null;
+        System.out.println(name);
+    }
+}

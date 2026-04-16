1b5083b7f5ae83d468b3a9dd3fe04fa16ada9e97
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-17 04:42:13 +0530

    Pretty Printing

--- src/stringDS/Output.java
+++ src/stringDS/Output.java
@@ -6,7 +6,7 @@ public class Output {
     public static void main(String[] args) {
         System.out.println(56);
 //        System.out.println("Ankita");
-        System.out.println(Arrays.toString(new int[]{1,2,4,5,3}));
+//        System.out.println(Arrays.toString(new int[]{1,2,4,5,3}));
 //        String name;
 //        System.out.println(name);
     }
---
+++ src/stringDS/PrettyPrinting.java
@@ -0,0 +1,8 @@
+package stringDS;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=453.1224f;
+        System.out.printf("Formatted number is %.2f", a);
+    }
+}

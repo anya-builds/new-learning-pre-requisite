a57014c0f78dc5bf6d933643d6076019b2e83d95
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-17 04:48:48 +0530

    operators

---
+++ src/stringDS/Operators.java
@@ -0,0 +1,9 @@
+package stringDS;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a'+'b');
+        System.out.println("a"+"b");
+        System.out.println((char)( 'a'+3));
+    }
+}
--- src/stringDS/PrettyPrinting.java
+++ src/stringDS/PrettyPrinting.java
@@ -4,5 +4,6 @@ public class PrettyPrinting {
     public static void main(String[] args) {
         float a=453.1224f;
         System.out.printf("Formatted number is %.2f", a);
+        System.out.printf("Pi number is %.3f", Math.PI);
     }
 }

0e3706de1d4391466e5b977df3efe2c7e1c2d1d5
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-18 15:36:53 +0530

    Recursion

--- src/Pattern/Main.java
+++ src/Pattern/Main.java
@@ -3,7 +3,7 @@ package Pattern;
 public class Main {
     public static void main(String[] args) {
 //            int n=4;
-            pattern4(4);
+            pattern5(5);
 //        System.out.println(pattern1(n));
     }
     static void pattern2(int n){
@@ -41,6 +41,17 @@ public class Main {
         for (int row = 1 ; row <=n; row++) {
             for (int col = 1; col <=row ; col++) {
                 System.out.print(col+" ");
+
+            }
+            System.out.println();
+
+        }
+    }
+    static void pattern5(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalColsInRow= row>n?2*n-row-1:row;
+            for (int col = 0; col <= totalColsInRow; col++) {
+                System.out.print("* ");
 
             }
             System.out.println();

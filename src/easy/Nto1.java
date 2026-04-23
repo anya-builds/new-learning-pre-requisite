0006514f399529e674559d87c7b0ccb592486f4d
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-23 22:07:08 +0530

    Recursion n to 1

--- src/easy/Nto1.java
+++ src/easy/Nto1.java
@@ -2,7 +2,7 @@ package easy;
 
 public class Nto1 {
     public static void main(String[] args) {
-        fun(5);
+        funRev(5);
     }
     static void fun(int n){
         if(n==0){
@@ -10,5 +10,13 @@ public class Nto1 {
         }
         System.out.print(n+" ");
         fun(n-1);
+    }
+    static void funRev(int n){
+        if(n==0){
+            return;
+        }
+        funRev(n-1);
+        System.out.print(n+" ");
+
     }
 }

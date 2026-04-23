42866626ab3e65a282d8d73b4a66e2dfa6f5017f
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-23 22:42:26 +0530

    Factorial using recursion

--- src/easy/Nto1.java
+++ src/easy/Nto1.java
@@ -2,7 +2,7 @@ package easy;
 
 public class Nto1 {
     public static void main(String[] args) {
-        funRev(5);
+        funBoth(5);
     }
     static void fun(int n){
         if(n==0){
@@ -16,6 +16,15 @@ public class Nto1 {
             return;
         }
         funRev(n-1);
+        System.out.print(n+" ");
+
+    }
+    static void funBoth(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n+" ");
+        funBoth(n-1);
         System.out.print(n+" ");
 
     }

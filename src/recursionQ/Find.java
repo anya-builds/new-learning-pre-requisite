3e63eafabd5dbe11a63131869235e4f7923ad890
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-25 14:51:21 +0530

    find index using recusrion

--- src/recursionQ/Find.java
+++ src/recursionQ/Find.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 public class Find {
     public static void main(String[] args) {
         int[] arr={2,3,1,4,4,5};
-        System.out.println(find(arr,4,0));
-        System.out.println(findIndex(arr,4,0));
-        System.out.println(findIndexLast(arr,4,arr.length-1));
-        findAllIndex(arr,4,0);
-        System.out.println(list);
+//        System.out.println(find(arr,4,0));
+//        System.out.println(findIndex(arr,4,0));
+//        System.out.println(findIndexLast(arr,4,arr.length-1));
+//        findAllIndex(arr,4,0);
+//        System.out.println(list);
+        System.out.println(findAllIndex2(arr,4,0));
     }
     static boolean find(int[] arr,int target,int index){
         if(index==arr.length){
@@ -46,5 +47,19 @@ public class Find {
             list.add(index);
         }
         findAllIndex(arr,target,index+1);
+    }
+
+    static ArrayList<Integer> findAllIndex2(int[] arr,int target,int index){
+        ArrayList<Integer> list=new ArrayList<>();
+        if(index==arr.length){
+            return list;
+        }
+        // this will contain answer for that function call only
+        if(arr[index]==target){
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls=findAllIndex2(arr,target,index+1);
+        list.addAll(ansFromBelowCalls);
+        return list;
     }
 }

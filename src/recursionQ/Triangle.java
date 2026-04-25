f3bed714a54efd8567ae3e583b9a5e3db9311429
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-25 15:50:53 +0530

    selection

--- src/recursionQ/Triangle.java
+++ src/recursionQ/Triangle.java
@@ -6,7 +6,7 @@ public class Triangle {
     public static void main() {
 //        triangle2(4,0);
         int[] arr={4,3,2,1};
-        bubble(arr,arr.length-1,0);
+        selection(arr,arr.length-1,0,0);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -48,6 +48,23 @@ public class Triangle {
             bubble(arr,r,c+1);
         }else{
             bubble(arr,r-1,0);
+        }
+    }
+    static void selection(int[] arr,int r,int c,int max){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(arr[c]>arr[max]){
+                selection(arr,r,c+1,c);
+            }else{
+                selection(arr,r,c+1,max);
+            }
+        }else{
+            int temp=arr[max];
+            arr[max]=arr[r-1];
+            arr[r-1]=temp;
+            selection(arr,r-1,0,0);
         }
     }
 }
